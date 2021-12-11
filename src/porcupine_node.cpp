d6e02c064cc75ec24adcaf9fb3e01b3cb0391093
Peter van Dooren <p.v.dooren@tue.nl>
2021-12-11 15:30:24 +0100

    make nodes

---
+++ src/porcupine_node.cpp
@@ -0,0 +1,19 @@
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "hmi_rhino");
+
+  ros::NodeHandle local_nh("~");
+
+  try
+  {
+    ros::spin();
+  }
+  catch (const std::exception& e)
+  {
+    ROS_FATAL("HmiPorcupineNode exception: %s", e.what());
+    return 1;
+  }
+  return 0;
+}
