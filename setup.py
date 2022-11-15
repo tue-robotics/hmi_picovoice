2ba6050ad1a9a8b87b60dc04b7f4a1dea8171d87
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-11-15 13:52:38 +0100

    chore(setup.py): apply black

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['hmi_picovoice'],
-    package_dir={'': 'src'},
+    packages=["hmi_picovoice"],
+    package_dir={"": "src"},
 )
 
 setup(**d)
